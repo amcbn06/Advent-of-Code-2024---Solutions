820274b1d851acc3573753b883d0ebbeb05f8f76
amcbn06 <89391344+amcbn06@users.noreply.github.com>
2024-12-01 10:50:19 +0200

    Uploaded part 1 of 01.12.2024

---
+++ solutions/01.12.2024/part1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ifstream fin(".in");
+
+int main() {
+	vector<int> a, b;
+	int x, y;
+	while (fin >> x >> y) {
+		a.push_back(x);
+		b.push_back(y);
+	}
+	assert(a.size() == b.size());
+	// We sort the arrays so that each on each index i, a[i] is paired with b[i]
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	// We compute the distance
+	int answer = 0;
+	for (int i = 0; i < a.size(); ++i) {
+		answer += abs(a[i] - b[i]);
+	}
+	cout << answer;
+}
