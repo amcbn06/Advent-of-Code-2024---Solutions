71cb0845447b3bdeddfd045e2443840cbd7b679e
amcbn06 <89391344+amcbn06@users.noreply.github.com>
2024-12-02 08:40:23 +0200

    Create part2.cpp

---
+++ solutions/day 2/part2.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+// Function to check if an array is safe
+bool safe(vector<int>& arr) {
+    if (arr.size() < 2) {
+        return true;
+	}
+    bool increasing = true, decreasing = false;
+	for (int i = 1; i < arr.size(); ++i) {
+		int delta = a[i] - a[i - 1];
+		increasing &= 1 <= delta && delta <= 3;
+		decreasing &= -3 <= delta && delta <= -1;
+	}
+	return increasing || decreasing;
+}
+
+int main() {
+	int answer = 0;
+	string line;
+	while (getline(fin, line)) {
+        stringstream ss(line);
+		vector<int> arr;
+		int x;
+		while (ss >> x) {
+			arr.push_back(x);
+		}
+		if (safe(arr)) {
+			answer++;
+		}
+		else {
+			for (int i = 0; i < arr.size(); ++i) {
+				vector<int> cpy = arr;
+				cpy.erase(cpy.begin() + i);
+				if (safe(cpy)) {
+					answer++;
+					break;
+				}
+			}
+		}
+	}
+	fout << answer;
+}
