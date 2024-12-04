fd74fb88be59d336735b172ab4856f6bf357c208
amcbn06 <89391344+amcbn06@users.noreply.github.com>
2024-12-04 07:27:27 +0200

    Update part2.cpp

--- solutions/day4/part2.cpp
+++ solutions/day4/part2.cpp
@@ -21,16 +21,16 @@ int main() {
 	n = a.size();
 	m = a[0].size();
 	int answer = 0;
-  // We pick a position for the top left corner
+	// We pick a position for the top left corner
 	for (int i = 0; i < n - 2; ++i) {
 		for (int j = 0; j < m - 2; ++j) {
-      // we extract the diagnoals of the 3x3 square corespondent to that corner
+    		// we extract the diagnoals of the 3x3 square corespondent to that corner
 			string d1, d2;
 			for (int k = 0; k < 3; ++k) {
 				d1 += a[i + k][j + k];
 				d2 += a[i + 2 - k][j + k];
 			}
-      // and we just check the condition
+      		// and we just check the condition
 			if ((d1 == "MAS" || d1 == "SAM") && (d2 == "MAS" || d2 == "SAM")) {
 				answer++;
 			}
