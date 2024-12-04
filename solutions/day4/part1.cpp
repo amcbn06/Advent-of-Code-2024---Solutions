9712795fec6eaddfcfc5d87d3bd9bec67cea3723
amcbn06 <89391344+amcbn06@users.noreply.github.com>
2024-12-04 07:28:37 +0200

    Update part1.cpp

--- solutions/day4/part1.cpp
+++ solutions/day4/part1.cpp
@@ -24,10 +24,10 @@ int main() {
 	// We pick a position for the first character
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
-      			// and a direction along which we search the word "XMAS"
+			// and a direction along which we search the word "XMAS"
 			for (int k = 0; k < 8; ++k) {
 				bool match = true;
-        			// and we just check the pattern
+				// and we just check the pattern
 				for (int pos = 0; pos < pattern.size(); ++pos) {
 					int ii = i + dx[k] * pos;
 					int jj = j + dy[k] * pos;
