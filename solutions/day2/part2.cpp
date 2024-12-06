bf6cc47470a56ad1c0bc5d1890b8db0bffc442df
amcbn06 <89391344+amcbn06@users.noreply.github.com>
2024-12-06 10:36:13 +0200

    Update part2.cpp

--- solutions/day2/part2.cpp
+++ solutions/day2/part2.cpp
@@ -8,7 +8,7 @@ bool safe(vector<int>& arr) {
     if (arr.size() < 2) {
         return true;
 	}
-    bool increasing = true, decreasing = true;
+	bool increasing = true, decreasing = true;
 	for (int i = 1; i < arr.size(); ++i) {
 		int delta = arr[i] - arr[i - 1];
 		increasing &= 1 <= delta && delta <= 3;
