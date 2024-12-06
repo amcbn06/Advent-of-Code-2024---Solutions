cf768a192c7406a14683e9f1d79a5f9cf6aaa94a
amcbn06 <89391344+amcbn06@users.noreply.github.com>
2024-12-06 10:35:30 +0200

    Update part1.cpp

--- solutions/day2/part1.cpp
+++ solutions/day2/part1.cpp
@@ -5,10 +5,10 @@ ofstream fout("output.txt");
 
 // Function to check if an array is safe
 bool safe(vector<int>& arr) {
-    if (arr.size() < 2) {
-        return true;
+	if (arr.size() < 2) {
+		return true;
 	}
-    bool increasing = true, decreasing = true;
+	bool increasing = true, decreasing = true;
 	for (int i = 1; i < arr.size(); ++i) {
 		int delta = arr[i] - arr[i - 1];
 		increasing &= 1 <= delta && delta <= 3;
